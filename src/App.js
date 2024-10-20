a310f9c9a42f3c0d18b43c5ac88816f81d7540a4
pyordanovdev <plamenyordanovdev@gmail.com>
2024-10-20 15:03:32 +0300

    Created the static version of FriendsList, Friend, and Button components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://avatar.iran.liara.run/public",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://avatar.iran.liara.run/public",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://avatar.iran.liara.run/public",
+    balance: 0,
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <FriendsList friendsList={initialFriends} />
     </div>
   );
+}
+
+function FriendsList({ friendsList }) {
+  return (
+    <div className='sidebar'>
+      <ul>
+        {friendsList &&
+          friendsList.length > 0 &&
+          friendsList.map((friend) => (
+            <Friend key={friend.id} friend={friend} />
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li key={friend.id}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p>You and Zoe are even</p>
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className='button'>{children}</button>;
 }
 
 export default App;
